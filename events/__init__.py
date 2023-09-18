a3a273e371de9852d8c5129b4631805d5a4e7281
Judy Jiang <judyjiang0808@gmail.com>
2023-09-18 13:55:15 -0700

    added login_check and joined function to show status message when they enter a room

---
+++ events/__init__.py
@@ -0,0 +1,36 @@
+from flask_socketio import SocketIO, emit
+from flask import session, g
+from model import User
+from model import ChatRoom
+from functools import wraps
+
+socketio = SocketIO(
+    async_mode='threading',
+    # transports=['websocket']
+)
+
+
+def login_check(f):
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        room = args[0].get('room')
+        if room is None:
+            return emit('status', {'code': -1, 'msg': 'room id missing', 'room': room})
+
+        room_obj: ChatRoom = ChatRoom.query.filter(ChatRoom.id == room).first()
+        g.room = room_obj
+        if room_obj is None:
+            return emit('status', {'code': -1, 'msg': 'room does not exist', 'room': room})
+
+        user_id = session.get('user_id')
+        if user_id:
+            user_obj: User = User.query.filter(User.user_id == user_id).first()
+            if user_obj is None:
+                return emit('status', {'code': -1, 'msg': 'invalid user', 'room': room})
+            else:
+                pass
+            g.user = user_obj
+        return f(*args, **kwargs)
+
+    return wrapper
+
---
+++ events/events.py
@@ -0,0 +1,30 @@
+from . import socketio, login_check
+from flask_socketio import emit
+from flask_socketio import SocketIO, emit, join_room
+
+from model import db, User, ChatRoom, Message, RoomMember, get_utc_timestamp
+
+
+@socketio.on('joined', namespace='/chat/')
+@login_check
+def joined(message):
+    """Sent by clients when they enter a room.
+    A status message is broadcasted to all people in the room."""
+
+    room_obj: ChatRoom = g.room
+    user_obj: User = g.user
+    member_info = RoomMember.ensure_membership(room_id=room_obj.id, user_id=user_obj.user_id)
+
+    join_room(room_obj.id)
+    print(
+        f'Enter [{room_obj.id}-{room_obj.category_name}-{room_obj.group_name}], {user_obj.user_id}-{user_obj.fname}-{user_obj.lname}')
+    emit(
+        'status',
+        {
+            'code': 3,
+            'kind': 'on-entrance',
+            'msg': f'{g.user.username} entered this chat',
+            'user': member_info
+        },
+        room=room_obj.id
+    )

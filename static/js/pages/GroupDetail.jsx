2ea9a1397668b9dbcb89770d17fe7a4c4a9cafca
Judy Jiang <judyjiang0808@gmail.com>
2023-09-18 19:34:15 -0700

    added RoomMember in GroupDetail.jsx

--- static/js/pages/GroupDetail.jsx
+++ static/js/pages/GroupDetail.jsx
@@ -17,3 +17,21 @@ let socket = null;
 const formatTime = (ts) => {
   return moment(ts).format("YYYY-MM-DD hh:mm:ss");
 };
+
+function RoomMember({
+  user_id,
+  fname,
+  lname,
+  email,
+  last_seen,
+  is_online,
+  current_user_id,
+  ...props
+}) {
+  let st = is_online ? <span>Online</span> : <span>Offline</span>;
+  return (
+    <div id={`member-${user_id}`}>
+      {fname} {lname} {current_user_id === user_id ? <span>Me</span> : st}
+    </div>
+  );
+}

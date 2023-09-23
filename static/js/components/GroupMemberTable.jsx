1c5a707bdf6ae6bfb9ce9bf8b932b033acd28727
Judy Jiang <judyjiang0808@gmail.com>
2023-09-23 13:13:54 -0700

    updated GroupMemberTable styling

--- static/js/components/GroupMemberTable.jsx
+++ static/js/components/GroupMemberTable.jsx
@@ -1,60 +1,68 @@
 const GroupMemberTable = ({ groupName, handleClosePopup }) => {
-  const [users, setUsers] = useState([]);
-
-  useEffect(() => {
-    fetch("/api/get-group-members", {
-      method: "POST",
-      body: JSON.stringify({
-        group_name: groupName,
-      }),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    })
-      .then((response) => response.json())
-      .then((data) => setUsers(data))
-      .catch((error) => console.error("Error fetching user data:", error));
-  }, []);
-  return (
-    <div>
-      <h3>Group Member Information</h3>
-      <button onClick={handleClosePopup} className="btn btn-primary mx-2">
-        Close Table
-      </button>
-      <div>
-        <table>
-          <thead>
-            <tr>
-              <th>#</th>
-              <th>User_ID</th>
-              <th>Username</th>
-              <th>First Name</th>
-              <th>Last Name</th>
-              <th>Gender</th>
-              <th>Age</th>
-              <th>Ethnicity</th>
-              <th>Occupation</th>
-              <th>Zipcode</th>
-            </tr>
-          </thead>
-          <tbody>
-            {users.map((user, index) => (
-              <tr key={user.user_id}>
-                <td>{index + 1}</td>
-                <td>{user.user_id}</td>
-                <td>{user.username}</td>
-                <td>{user.fname}</td>
-                <td>{user.lname}</td>
-                <td>{user.gender}</td>
-                <td>{user.age}</td>
-                <td>{user.ethnicity}</td>
-                <td>{user.occupation}</td>
-                <td>{user.zipcode}</td>
+    const [users, setUsers] = useState([]);
+    
+  
+    useEffect(() => {
+      fetch("/api/get-group-members", {
+        method: "POST",
+        body: JSON.stringify({
+          group_name: groupName,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+        .then((response) => response.json())
+        .then((data) => setUsers(data))
+        .catch((error) => console.error("Error fetching user data:", error));
+    }, []);
+  
+    
+    };
+  
+    return (
+      <div className="user-table-container">
+        <h3>Group Member Information</h3>
+        <button onClick={handleClosePopup} className="btn btn-primary mx-2">
+          Close Table
+        </button>
+        <div className="table-wrapper">
+          <table>
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>User_ID</th>
+                <th>Username</th>
+                <th>
+                  First Name 
+                </th>
+                <th>Last Name</th>
+                <th>Gender</th>
+                <th>Age</th>
+                <th>Ethnicity</th>
+                <th>Occupation</th>
+                <th>Zipcode</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {users.map((user, index) => (
+                <tr key={user.user_id}>
+                  <td>{index + 1}</td>
+                  <td>{user.user_id}</td>
+                  <td>{user.username}</td>
+                  <td>{user.fname}</td>
+                  <td>{user.lname}</td>
+                  <td>{user.gender}</td>
+                  <td>{user.age}</td>
+                  <td>{user.ethnicity}</td>
+                  <td>{user.occupation}</td>
+                  <td>{user.zipcode}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  };
+  

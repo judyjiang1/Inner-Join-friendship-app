b1a74ff6a0242b8712b49d3eb1b4d852656348ba
Judy Jiang <judyjiang0808@gmail.com>
2023-09-23 13:10:47 -0700

    updated GroupMemberTable

--- static/js/components/GroupMemberTable.jsx
+++ static/js/components/GroupMemberTable.jsx
@@ -1,6 +1,5 @@
 const GroupMemberTable = ({ groupName, handleClosePopup }) => {
   const [users, setUsers] = useState([]);
-  const [sortOrder, setSortOrder] = useState("asc");
 
   useEffect(() => {
     fetch("/api/get-group-members", {
@@ -16,4 +15,46 @@ const GroupMemberTable = ({ groupName, handleClosePopup }) => {
       .then((data) => setUsers(data))
       .catch((error) => console.error("Error fetching user data:", error));
   }, []);
+  return (
+    <div>
+      <h3>Group Member Information</h3>
+      <button onClick={handleClosePopup} className="btn btn-primary mx-2">
+        Close Table
+      </button>
+      <div>
+        <table>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>User_ID</th>
+              <th>Username</th>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>Gender</th>
+              <th>Age</th>
+              <th>Ethnicity</th>
+              <th>Occupation</th>
+              <th>Zipcode</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user, index) => (
+              <tr key={user.user_id}>
+                <td>{index + 1}</td>
+                <td>{user.user_id}</td>
+                <td>{user.username}</td>
+                <td>{user.fname}</td>
+                <td>{user.lname}</td>
+                <td>{user.gender}</td>
+                <td>{user.age}</td>
+                <td>{user.ethnicity}</td>
+                <td>{user.occupation}</td>
+                <td>{user.zipcode}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };

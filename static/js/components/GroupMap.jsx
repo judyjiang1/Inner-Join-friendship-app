eef05e565585dcb68fcaf5e567d01ba824a86c42
Judy Jiang <judyjiang0808@gmail.com>
2023-09-23 17:15:47 -0700

    added feature to group map where users can click on each marker to see user info

--- static/js/components/GroupMap.jsx
+++ static/js/components/GroupMap.jsx
@@ -7,28 +7,73 @@ function initMap(users) {
   const groupedUsers = {};
 
   users.forEach((user) => {
-    const { zipcode, username } = user;
+    const {
+      user_id,
+      username,
+      fname,
+      lname,
+      gender,
+      age,
+      ethnicity,
+      occupation,
+      zipcode,
+    } = user;
 
     if (!groupedUsers[zipcode]) {
       groupedUsers[zipcode] = [];
     }
 
-    groupedUsers[zipcode].push(username);
+    groupedUsers[zipcode].push({
+      user_id,
+      username,
+      fname,
+      lname,
+      gender,
+      age,
+      ethnicity,
+      occupation,
+      zipcode,
+    });
   });
 
-  for (const [zipcode, usernames] of Object.entries(groupedUsers)) {
+  for (const [zipcode, userArray] of Object.entries(groupedUsers)) {
     const geocoder = new window.google.maps.Geocoder();
     geocoder.geocode({ address: zipcode }, (results, status) => {
       if (status === "OK" && results[0]) {
         const { lat, lng } = results[0].geometry.location;
 
-        new window.google.maps.Marker({
+        const labelUserContent = userArray
+          .map((item) => item.username)
+          .join(" & ");
+
+        const marker = new window.google.maps.Marker({
           position: { lat: lat(), lng: lng() },
           map,
           label: {
-            text: `${usernames.join(" & ")}`,
+            text: labelUserContent,
             color: "#4B0082",
+            fontWeight: "bold",
           },
+        });
+
+        const infoWindow = new window.google.maps.InfoWindow();
+
+        marker.addListener("click", () => {
+          const infoContent = userArray
+            .map(
+              (item) =>
+                `<strong>User ID:</strong> ${item.user_id}<br>` +
+                `<strong>Username:</strong> ${item.username}<br>` +
+                `<strong>First Name:</strong> ${item.fname}<br>` +
+                `<strong>Last Name:</strong> ${item.lname}<br>` +
+                `<strong>Gender:</strong> ${item.gender}<br>` +
+                `<strong>Age:</strong> ${item.age}<br>` +
+                `<strong>Ethnicity:</strong> ${item.ethnicity}<br>` +
+                `<strong>Zip Code:</strong> ${item.zipcode}`
+            )
+            .join("<br><br>");
+          infoWindow.setContent(infoContent);
+          infoWindow.open(map, marker);
         });
       } else {
         // alert("Geocode was not successful for the following reason: " + status);
